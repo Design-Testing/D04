d85256cfb81e16be31a8ccfbf26c6d67c23bca73
Juan Morón <jmoronvilan@gmail.com>
2019-04-30 18:58:03 +0200

    Audit populate & domain

---
+++ Item 3/Acme-Rookies/src/main/java/domain/Audit.java
@@ -0,0 +1,74 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.SafeHtml;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Audit extends DomainEntity {
+
+	//Relationships
+	private String		text;
+	private Integer		score;
+	private Date		moment;
+
+	private Position	position;
+
+
+	@NotBlank
+	@SafeHtml
+	public String getText() {
+		return this.text;
+	}
+
+	public void setText(final String text) {
+		this.text = text;
+	}
+
+	@NotNull
+	@Range(min = 0, max = 10)
+	public Integer getScore() {
+		return this.score;
+	}
+
+	public void setScore(final Integer score) {
+		this.score = score;
+	}
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+	public Date getMoment() {
+		return this.moment;
+	}
+
+	public void setMoment(final Date moment) {
+		this.moment = moment;
+	}
+
+	@Valid
+	@ManyToOne(optional = false)
+	public Position getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(final Position position) {
+		this.position = position;
+	}
+
+}
