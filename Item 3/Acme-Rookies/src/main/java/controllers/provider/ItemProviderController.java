dbad823d938d564d1de851a79ba0732ddf4a0562
albaolmo <albaolmotrinidad@gmail.com>
2019-05-05 16:47:54 +0200

    item controller

--- Item 3/Acme-Rookies/src/main/java/controllers/provider/ItemProviderController.java
+++ Item 3/Acme-Rookies/src/main/java/controllers/provider/ItemProviderController.java
@@ -9,16 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import security.Authority;
+import security.LoginService;
+import security.UserAccount;
 import services.ItemService;
 import services.ProviderService;
 import controllers.AbstractController;
 import domain.Item;
+import domain.Provider;
 
 @Controller
 @RequestMapping("/item/provider")
@@ -40,8 +43,19 @@ public class ItemProviderController extends AbstractController {
 		ModelAndView result;
 		Collection<Item> items;
 
-		items = this.itemService.findAllByProvider();
+		final UserAccount uaLogged = LoginService.getPrincipal();
+		final Authority auProvider = new Authority();
+		auProvider.setAuthority("PROVIDER");
+
 		result = new ModelAndView("item/list");
+
+		if (uaLogged.getAuthorities().contains(auProvider)) {
+			final Provider provider = this.providerService.findByPrincipal();
+			items = this.itemService.findAllByProvider(provider.getId());
+			result.addObject("buttons", true);
+		} else
+			items = this.itemService.findAll();
+
 		result.addObject("items", items);
 		result.addObject("lang", this.lang);
 		result.addObject("requestURI", "/item/provider/list.do");
@@ -49,19 +63,32 @@ public class ItemProviderController extends AbstractController {
 
 	}
 
+	@RequestMapping(value = "/listByProvider", method = RequestMethod.GET)
+	public ModelAndView listByProvider(@RequestParam final int providerId) {
+		ModelAndView result;
+		Collection<Item> items;
+
+		result = new ModelAndView("item/list");
+
+		items = this.itemService.findAllByProvider(providerId);
+
+		result.addObject("items", items);
+		result.addObject("lang", this.lang);
+		result.addObject("requestURI", "/item/provider/listByProvider.do");
+		return result;
+
+	}
+
 	// =================CREATE=================
 
 	@RequestMapping(value = "/create", method = RequestMethod.GET)
-	public ModelAndView create(@RequestParam final int paradeId) {
+	public ModelAndView create() {
 		final ModelAndView result;
 		Item item;
-		Parade parade;
-
-		parade = this.paradeService.findOne(paradeId);
 
 		item = this.itemService.create();
-		result = this.createEditModelAndView(item);
-		result.addObject("parade", parade);
+		result = new ModelAndView("item/edit");
+		result.addObject("item", item);
 
 		return result;
 	}
@@ -73,101 +100,53 @@ public class ItemProviderController extends AbstractController {
 		ModelAndView result;
 		final Item item;
 
+		final UserAccount uaLogged = LoginService.getPrincipal();
+		final Authority auProvider = new Authority();
+		auProvider.setAuthority("PROVIDER");
+
 		item = this.itemService.findOne(itemId);
 
-		if (item != null) {
-			final Collection<Item> ss = this.itemService.findAllByProvider();
-			if (ss.contains(item))
-				result = this.createEditModelAndView(item);
-			else
+		if ((item != null) && (uaLogged.getAuthorities().contains(auProvider))) {
+			final Provider provider = this.providerService.findByPrincipal();
+			final Collection<Item> ss = this.itemService.findAllByProvider(provider.getId());
+			if (ss.contains(item)) {
+				result = new ModelAndView("item/edit");
+				result.addObject("item", item);
+			} else
 				result = new ModelAndView("redirect:/misc/403.jsp");
 		} else
 			result = new ModelAndView("redirect:/misc/403.jsp");
-
 		return result;
 	}
 
 	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
-	public ModelAndView save(@Valid @ModelAttribute("item") final ItemForm itemForm, final BindingResult binding) {
+	public ModelAndView save(@Valid final Item item, final BindingResult bindingResult) {
+
 		ModelAndView result;
-		Item item;
+		if (bindingResult.hasErrors()) {
+			result = new ModelAndView("item/edit");
+			result.addObject("item", item);
+		} else
+			try {
+				this.itemService.save(item);
 
-		try {
-			item = this.itemService.reconstruct(itemForm, binding);
-			this.itemService.save(item);
-			result = new ModelAndView("redirect:list.do");
-		} catch (final ValidationException oops) {
-			result = this.createEditModelAndView(itemForm);
-		} catch (final Throwable oops) {
-			String errorMessage = "item.commit.error";
-			if (oops.getMessage().contains("message.error"))
-				errorMessage = oops.getMessage();
-			result = this.createEditModelAndView(itemForm, errorMessage);
-		}
-		return result;
-	}
+				result = new ModelAndView("item/list");
 
-	// =================REACTIVATE=================
+			} catch (final Throwable e) {
+				result = new ModelAndView("redirect:misc/403");
+			}
 
-	@RequestMapping(value = "/reactivate", method = RequestMethod.GET)
-	public ModelAndView activate(@RequestParam final int itemId) {
-		ModelAndView result;
-		final Item item;
-
-		item = this.itemService.findOne(itemId);
-
-		try {
-			this.itemService.reactivate(itemId);
-			result = new ModelAndView("redirect:list.do");
-		} catch (final Throwable oops) {
-			result = this.createEditModelAndView(item, "item.commit.error");
-		}
-		return result;
-	}
-
-	// =================DEACTIVATE=================
-
-	@RequestMapping(value = "/deactivate", method = RequestMethod.GET)
-	public ModelAndView deactivate(@RequestParam final int itemId) {
-		ModelAndView result;
-		final Item item;
-
-		item = this.itemService.findOne(itemId);
-
-		try {
-			this.itemService.deactivate(itemId);
-			result = new ModelAndView("redirect:list.do");
-		} catch (final Throwable oops) {
-			result = this.createEditModelAndView(item, "item.commit.error");
-		}
 		return result;
 	}
 
 	// =================DISPLAY=================
 
 	@RequestMapping(value = "/display", method = RequestMethod.GET)
-	public ModelAndView display(@RequestParam final int paradeId) {
-		final ModelAndView result;
-		Item item;
-		final Provider provider = this.providerService.findByPrincipal();
-
-		item = this.itemService.findByParade(paradeId, provider.getUserAccount().getId());
-
-		if (item != null) {
-			result = new ModelAndView("item/display");
-			result.addObject("item", item);
-		} else
-			result = new ModelAndView("redirect:/misc/403.jsp");
-
-		return result;
-	}
-
-	@RequestMapping(value = "/displayItem", method = RequestMethod.GET)
 	public ModelAndView displayItem(@RequestParam final int itemId) {
 		final ModelAndView result;
 		Item item;
 
-		item = this.itemService.findOneItem(itemId);
+		item = this.itemService.findOne(itemId);
 
 		if (item != null) {
 			result = new ModelAndView("item/display");
@@ -178,55 +157,4 @@ public class ItemProviderController extends AbstractController {
 		return result;
 	}
 
-	//ANCILLIARY METHODS
-
-	protected ModelAndView createEditModelAndView(final ItemForm item) {
-		ModelAndView result;
-
-		result = this.createEditModelAndView(item, null);
-
-		return result;
-	}
-
-	protected ModelAndView createEditModelAndView(final ItemForm item, final String messageCode) {
-		final ModelAndView result;
-
-		final Collection<Parade> parades = this.paradeService.paradesAvailableProvider();
-
-		result = new ModelAndView("item/edit");
-		result.addObject("item", item);
-		result.addObject("parades", parades);
-		//result.addObject("requestURI", "/item/provider/edit.do?itemId=" + item.getId());
-		result.addObject("message", messageCode);
-		result.addObject("makes", this.configurationParametersService.find().getCreditCardMake());
-		return result;
-	}
-
-	protected ModelAndView createEditModelAndView(final Item item) {
-		ModelAndView result;
-
-		result = this.createEditModelAndView(item, null);
-
-		return result;
-	}
-
-	protected ModelAndView createEditModelAndView(final Item item, final String messageCode) {
-		final ModelAndView result;
-		ItemForm itemForm;
-
-		if (item.getId() == 0)
-			itemForm = new ItemForm();
-		else
-			itemForm = this.itemService.inyect(item);
-
-		final Collection<Parade> parades = this.paradeService.paradesAvailableProvider();
-
-		result = new ModelAndView("item/edit");
-		result.addObject("item", itemForm);
-		result.addObject("parades", parades);
-		//result.addObject("requestURI", "/item/provider/edit.do?itemId=" + item.getId());
-		result.addObject("makes", this.configurationParametersService.find().getCreditCardMake());
-		result.addObject("message", messageCode);
-		return result;
-	}
 }
